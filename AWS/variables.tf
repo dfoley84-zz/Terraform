86e692db5011165c4686cb6296e01a821df4fa31
dfoley84 <david.foley@mycit.ie>
2017-11-22 19:55:56 +0000

    Add files via upload

---
+++ AWS/variables.tf
@@ -0,0 +1,8 @@
+variable "region"{}
+variable "aws_profile"{}
+variable "key_name"{}
+variable "instance_class"{}
+variable "aws_subnet"{}
+variable "ami_type"{}
+variable "security_group"{}
+variable "S3"{}
