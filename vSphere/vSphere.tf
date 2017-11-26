9b88a53cd7c584e4d2551941098fcf54494aefd6
dfoley84 <david.foley@mycit.ie>
2017-11-26 22:56:33 +0000

    Update vSphere.tf

--- vSphere/vSphere.tf
+++ vSphere/vSphere.tf
@@ -9,8 +9,14 @@ resource "vsphere_virtual_machine" "web" {
   name =  "web-1"
   vcpu = 4
   memory = 12096
+  domain = "${var.domain}"
+  cluster = "${var.production_cluster}"
   
   network_interface {
     label = "${var.Production}"
-    }
+    ipv4_address       = "172.27.0.151"
+    ipv4_prefix_length = "24"
+    ipv4_gateway       = "${var.cix_gateway}"
+ }
+ 
 }
