f18758ee0668dfbf84b258f58f67e3cc03f87ae3
dfoley84 <david.foley@mycit.ie>
2017-11-26 23:13:30 +0000

    Add files via upload

---
+++ vSphere/variables.tf
@@ -0,0 +1,7 @@
+variable "vsphere_user"{}
+variable "vsphere_server"{}
+variable "vsphere_password"{}
+variable "vphere_datacenter"{}
+variable "production_cluster"{}
+variable "cix_gateway"{}
+variable "Web_Template"{}
