b88890b5824ba0722248860da0ecc0898f1312b7
dfoley84 <david.foley@mycit.ie>
2017-11-27 10:39:46 +0000

    Update vSphere.tf

--- vSphere/vSphere.tf
+++ vSphere/vSphere.tf
@@ -1,18 +1,15 @@
-provider "vsphere" {
+# Create a Virutal Machine from a Clone Instance.
+
+provider "vsphere"
+ {
   user           = "${var.vsphere_user}"
   password       = "${var.vsphere_password}"
   vsphere_server = "${var.vsphere_server}"
   allow_unverified_ssl = true
 }
 
-resource "vsphere_folder" "WebFolder" {
-  datacenter = "${var.vphere_datacenter}"
-  type = "vm"
-  path = "${var.path}"
-}
 resource "vsphere_virtual_machine" "web" {
   name =  "web-1"
-  folder = "${vsphere_folder.webFolder.path}"
   vcpu = 4
   memory = 12096
   datacenter = "${var.vphere_datacenter}"
@@ -21,11 +18,12 @@ resource "vsphere_virtual_machine" "web" {
 
   network_interface {
     label = "${var.Production}"
-    ipv4_address       = ""
+    ipv4_address       = "<< IP ADDRESS>>"
     ipv4_prefix_length = "24"
     ipv4_gateway       = "${var.cix_gateway}"
  }
  disk {
-  template = "${var.Web_Template}"
+  datastore = "${var.raid5_datastore}"
+  template = "${var.webtemplate}"
  }
 }
