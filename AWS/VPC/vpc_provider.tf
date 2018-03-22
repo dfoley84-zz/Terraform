475de20c0d9a5c136ff641a0c6b88de99260e442
dfoley84 <david.foley@mycit.ie>
2018-03-22 15:23:17 +0000

    Update vpc_provider.tf

--- AWS/VPC/vpc_provider.tf
+++ AWS/VPC/vpc_provider.tf
@@ -1,70 +1,158 @@
-# Using Terraform AWS Provider to Create a VPC Network and Subnet within AWS
-
-provider "aws"
-{
-  region = "${var.region}"
-  profile = "${var.aws_profile}"
+provider "aws" {
+    region = "${var.aws_region_Dublin}"
+    profile ="${var.aws_profile}"
 }
 
-# Creating VPC
-resource "aws_vpc" "vpc"
-{
-  cidr_block = "172.27.0.0/16"
+# VPC 
+resource "aws_vpc" "Dublin_vpc" {
+  cidr_block = "172.23.0.0/16"
+  tags{
+      Name = "Dublin_VPC"
+  }
 }
 
-# Creating internet gateway
-resource "aws_internet_gateway" "internet_gateway"
-{
-  vpc_id = "${aws_vpc.vpc.id}"
+# Gateway
+
+resource "aws_internet_gateway" "Dublin_internet_gateway" {
+    vpc_id = "${aws_vpc.Dublin_vpc.id}"
+
+    tags {
+        Name = "Dublin_igw"
+    }
 }
 
-# Creating a Public Route
-resource "aws_route_table" "public"
-{
-  vpc_id = "${aws_vpc.vpc.id}"
-  route
-  {
-        cidr_block = "0.0.0.0/0"
-	gateway_id = "${aws_internet_gateway.internet_gateway.id}"
+# Route Table Public
+
+resource "aws_route_table" "Dublin_public_rt" {
+  vpc_id = "${aws_vpc.Dublin_vpc.id}"
+  route {
+    cidr_block = "0.0.0.0/0"
+	  gateway_id = "${aws_internet_gateway.Dublin_internet_gateway.id}"
 	}
   tags {
-	Name = "Public Route"
+	Name = "Dublin Public Route"
   }
 }
 
-#Creating a Private Route
-resource "aws_default_route_table" "private"
-{
-  default_route_table_id = "${aws_vpc.vpc.default_route_table_id}"
+# Route Table Private
+
+resource "aws_default_route_table" "Dublin_private_rt" {
+  default_route_table_id = "${aws_vpc.Dublin_vpc.default_route_table_id}"
   tags {
-    Name = "Private Route"
+    Name = "Dublin Private Route"
   }
 }
 
-#Creating a Public Subnet
-resource "aws_subnet" "public"
- {
-  vpc_id = "${aws_vpc.vpc.id}"
-  cidr_block = "172.27.1.0/24"
+
+# Public Subnet
+
+resource "aws_subnet" "Dublin_public_subnet" {
+  vpc_id = "${aws_vpc.Dublin_vpc.id}"
+  cidr_block = "${var.cidrs["public1"]}"
   map_public_ip_on_launch = true
-#  availability_zone = "${data.aws_availability_zones.available.names[2]}"
+  availability_zone = "${data.aws_availability_zones.available.names[0]}"
 
   tags {
-    Name = "public Subnet"
+    Name = "Dublin public Subnet"
   }
 }
 
-#Creating a Private Subnet
-resource "aws_subnet" "private"
-{
-  vpc_id = "${aws_vpc.vpc.id}"
-  cidr_block = "172.27.2.0/24"
+# Private Subnet
+
+resource "aws_subnet" "Dublin_private_subnet" {
+  vpc_id = "${aws_vpc.Dublin_vpc.id}"
+  cidr_block = "${var.cidrs["private1"]}"
   map_public_ip_on_launch = false
-  #availability_zone = "${data.aws_availability_zones.available.names[0]}"
+ availability_zone = "${data.aws_availability_zones.available.names[0]}"
   tags {
-    Name = "private subnet"
+    Name = "Dublin private subnet"
   }
 }
 
-#Setting up ACL / WAF Environment
-#resource ""
+# Subnet Association
+
+resource "aws_route_table_association" "Dublin_public_assoc" {
+  subnet_id = "${aws_subnet.Dublin_public_subnet.id}"
+  route_table_id = "${aws_route_table.Dublin_public_rt.id}"
+}
+
+resource "aws_route_table_association" "Dublin_private_assoc" {
+    subnet_id = "${aws_subnet.Dublin_private_subnet.id}"
+    route_table_id = "${aws_default_route_table.Dublin_private_rt.id}"
+}
+
+
+
+# Public Security Groups 
+
+resource "aws_security_group" "Dublin_Development_SG" {
+  name = "Dublin_Development_SG"
+  description = "Access to Development Instance"
+  vpc_id =  "${aws_vpc.Dublin_vpc.id}"
+
+  #SSH
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["${var.SP_cidrs["work_ipaddress"]}","${var.SP_cidrs["home_ipaddress"]}"]
+
+  }
+
+  #HTTP
+  ingress {
+    from_port = 80 
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["${var.SP_cidrs["work_ipaddress"]}","${var.SP_cidrs["home_ipaddress"]}"]
+  }
+
+egress {
+  from_port = 0
+  to_port = 0
+  protocol = "-1"
+  cidr_blocks =["0.0.0.0/0"]
+}
+}
+
+resource "aws_security_group" "Dublin_public_SG" {
+  name = "Dublin_public_SG"
+  description = "Used for the ELB for public Access"
+  vpc_id =  "${aws_vpc.Dublin_vpc.id}"
+
+  #HTTP 
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks =["0.0.0.0/0"]
+  }
+egress {
+  from_port = 0
+  to_port = 0
+  protocol = "-1"
+  cidr_blocks =["0.0.0.0/0"]
+}
+}
+
+# Private Security Group
+resource "aws_security_group" "Dublin_private_sg" {
+  name = "Dublin_private_sg"
+  description = "Used for Private EC2"
+  vpc_id = "${aws_vpc.Dublin_vpc.id}"
+}
+
+# Access From VPC
+ingress {
+  from_port = 0
+  to_port = 0
+  protocol = "-1"
+  cidr_blocks = ["${var.vpc_cidr}"]
+}
+egress {
+  from_port = 0
+  to_port = 0
+  protocol = "-1"
+   cidr_blocks =["0.0.0.0/0"]
+}
+}
