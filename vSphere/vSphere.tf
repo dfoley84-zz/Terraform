71f9ff4a5b9c1bbcbb5ad2d78842a10307e27d1d
dfoley84 <david.foley@mycit.ie>
2018-07-17 13:57:51 +0100

    Update vSphere.tf

--- vSphere/vSphere.tf
+++ vSphere/vSphere.tf
@@ -1,29 +1,32 @@
-# Create a Virutal Machine from a Clone Instance.
+#Creating Server from Clone Instance 
 
 provider "vsphere"
  {
-  user           = "${var.vsphere_user}"
-  password       = "${var.vsphere_password}"
-  vsphere_server = "${var.vsphere_server}"
+  user           = "${var.Username}"
+  password       = "${var.Password}"
+  vsphere_server = "${var.Server}"
   allow_unverified_ssl = true
 }
+resource "vsphere_folder" "Terraform_Folder" {
+    datacenter = "${var.vphere_datacenter}"
+    path = "${var.vRealizeFolder}"
+}
 
-resource "vsphere_virtual_machine" "web" {
-  name =  "web-1"
-  vcpu = 4
-  memory = 12096
-  datacenter = "${var.vphere_datacenter}"
-  domain = "${var.domain}"
-  cluster = "${var.production_cluster}"
-
-  network_interface {
-    label = "${var.Production}"
-    ipv4_address       = "<< IP ADDRESS>>"
+resource "vsphere_virtual_machine" "Production Server" {
+    name =  "${var.prdouction_prefix} + Test123"
+    folder = "${vsphere_folder.Terraform_Folder.path}"
+    cluster = "${var.production_cluster}"
+    vcpu = 4
+    memory = 12096  
+  
+network_interface{
+    label = "${var.vlan105}"
+    ipv4_address       = "10.40.105.225"
     ipv4_prefix_length = "24"
-    ipv4_gateway       = "${var.cix_gateway}"
- }
- disk {
-  datastore = "${var.raid5_datastore}"
-  template = "${var.webtemplate}"
- }
+    ipv4_gateway       = "10.40.105.100"
+}
+disk {
+  datastore = "${var.storage}"
+  template = "${var.Windows}"
+}
 }
