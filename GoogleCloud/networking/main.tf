00155bea2a4659484e307b7420b46ca9909c2945
dfoley84 <david.foley@mycit.ie>
2018-09-28 16:41:21 +0100

    Update main.tf

--- GoogleCloud/networking/main.tf
+++ GoogleCloud/networking/main.tf
@@ -1,16 +1,78 @@
 provider "google" {
-  credentials = "${file("credentials.json")}"
+  credentials = "${file("linux-academy-216521-f3588c152db3.json")}"
   project     = "${var.Google_Project}"
   region      = "${var.Region}"
 }
 
-resource "google_compute_network" "Project_Network" {
-  name = "Linux Project"
-  project = "${var.project}"
-  
+resource "google_compute_network" "vpc" {
+ name                    = "${var.vpc_name}"
+ auto_create_subnetworks = "false"
+}
+
+resource "google_compute_subnetwork" "subnet" {
+ name          = "${var.vpc_name}"
+ ip_cidr_range = "${var.subnet_cidr}"
+ network       = "${var.vpc_name}"
+ depends_on    = ["google_compute_network.vpc"]
+ region      = "${var.Region}"
+}
+
+resource "google_compute_firewall" "firewall" {
+  name    = "${var.vpc_name}-allow-ssh"
+  network = "${google_compute_network.vpc.name}"
+  allow {
+    protocol = "icmp"
+  }
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_firewall" "http" {
+  name    = "${var.vpc_name}-allow-http"
+  network = "${google_compute_network.vpc.name}"
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+  source_ranges = ["0.0.0.0/0"]
 }
 
 
+resource "google_compute_firewall" "https" {
+  name    = "${var.vpc_name}-allow-https"
+  network = "${google_compute_network.vpc.name}"
+  allow {
+    protocol = "tcp"
+    ports    = ["443"]
+  }
+  source_ranges = ["0.0.0.0/0"]
+}
 
+resource "google_compute_firewall" "tomcat" {
+  name    = "${var.vpc_name}-allow-tomcat"
+  network = "${google_compute_network.vpc.name}"
+  allow {
+    protocol = "icmp"
+  }
+  allow {
+    protocol = "tcp"
+    ports    = ["8080"]
+  }
+  source_ranges = ["172.27.10.0/24"]
+}
 
-
+resource "google_compute_firewall" "vnc" {
+  name    = "${var.vpc_name}-allow-vnc"
+  network = "${google_compute_network.vpc.name}"
+  allow {
+    protocol = "icmp"
+  }
+  allow {
+    protocol = "tcp"
+    ports    = ["5901"]
+  }
+  source_ranges = ["172.27.10.0/24"]
+}
