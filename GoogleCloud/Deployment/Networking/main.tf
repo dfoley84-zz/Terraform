f2225c2607e1f41e63437008069e0a22eb48b032
dfoley84 <david.foley@mycit.ie>
2019-11-25 16:25:16 +0000

    Update main.tf
    Updated Firewall Rules for Kibana and ElasticSearch

--- GoogleCloud/Deployment/Networking/main.tf
+++ GoogleCloud/Deployment/Networking/main.tf
@@ -1,9 +1,9 @@
+# Create GCP VPC
 provider "google" {
-  credentials = "${file(".json")}"
+  #credentials = "${file("playground-s-11-8d9435-3bdb2307bf78.json")}"
   project     = "${var.project_name}"
 }
 
-#VPC
 
 resource "google_compute_network" "vpc" {
  name                    = "${var.vpc_name}"
@@ -12,7 +12,6 @@ resource "google_compute_network" "vpc" {
 }
 
 # Create GCP Subnet 
-
 resource "google_compute_subnetwork" "subnet" {
  name          = "${var.subnet_name}"
  region      = "${var.region["London"]}"
@@ -22,7 +21,6 @@ resource "google_compute_subnetwork" "subnet" {
 }
 
 # GCP Router for NAT Instance
-
 resource "google_compute_router" "router" {
   name    = "${var.router_name}"
   network = google_compute_network.vpc.self_link
@@ -30,7 +28,6 @@ resource "google_compute_router" "router" {
 }
 
 # GCP NAT for Private Hosts
-
 resource "google_compute_router_nat" "nat" {
   name                               = "${var.nat_name}"
   router                             = google_compute_router.router.name
@@ -46,10 +43,10 @@ resource "google_compute_router_nat" "nat" {
 
 #Firewall Rules
 
-resource "google_compute_firewall" "firewall" {
+resource "google_compute_firewall" "ssh" {
   name    = "${var.vpc_name}-allow-ssh"
   network = "${google_compute_network.vpc.name}"
-  source_tags = ["bastion_host"]
+  source_tags = ["bastion"]
   allow {
     protocol = "icmp"
   }
@@ -58,4 +55,36 @@ resource "google_compute_firewall" "firewall" {
     ports    = ["22"]
   }
   source_ranges = ["${var.local_cidrs["IE_Office"]}", "${var.local_cidrs["UK_Office"]}"]
+}
+
+resource "google_compute_firewall" "saltstack" {
+  name    = "${var.vpc_name}-allow-saltstack"
+  network = "${google_compute_network.vpc.name}"
+  source_tags = ["bastion","private"]
+  allow {
+    protocol = "tcp"
+    ports    = ["4505","4506"]
+  }
+  source_ranges = ["${var.cidrs["London"]}"]
+}
+
+resource "google_compute_firewall" "elkstack" {
+  name    = "${var.vpc_name}-allow-elkstack"
+  network = "${google_compute_network.vpc.name}"
+  source_tags = ["private"]
+  allow {
+    protocol = "tcp"
+    ports    = ["9200","5601"]
+  }
+  source_ranges = ["${var.cidrs["London"]}"]
+}
+
+resource "google_compute_firewall" "kibana" {
+  name    = "${var.vpc_name}-allow-external-kibana"
+  network = "${google_compute_network.vpc.name}"
+  allow {
+    protocol = "tcp"
+    ports    = ["5601"]
+  }
+  source_ranges = ["${var.local_cidrs["IE_Office"]}","${var.local_cidrs["UK_Office"]}"]
 }
